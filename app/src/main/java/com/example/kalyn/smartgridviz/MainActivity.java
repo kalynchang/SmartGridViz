ccba40aa011f9cc91406872e7c188eb7bd770d8a
Ker Lee Yap <kl.yap@hotmail.com>
2015-01-04 22:05:23 -0800

    Hooked up buttons
    All 3 menu buttons work and switch the menu fragment to each button's
    corresponding fragment

--- app/src/main/java/com/example/kalyn/smartgridviz/MainActivity.java
+++ app/src/main/java/com/example/kalyn/smartgridviz/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.kalyn.smartgridviz;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,7 +17,13 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.LinearLayout, new MenuFragment())
+                    .commit();
+        }
     }
 
 
@@ -25,7 +37,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
+        // automatically handle clicks on the Home/Up aboutbutton, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
@@ -35,5 +47,81 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class MenuFragment extends Fragment {
+        Button aboutbutton;
+        Button processbutton;
+        Button peoplebutton;
+        private final String TAG = MenuFragment.class.getSimpleName();
+        /**
+         * Displays a Fragment.
+         * @param f     The Fragment to be displayed.
+         * @param tag   The associated tag that is with the Fragment, usually the class name.
+         */
+        public void displayFragment(Fragment f, String tag) {
+            FragmentTransaction ft = getFragmentManager().beginTransaction()
+                    .addToBackStack(tag);
+            ft.replace(R.id.LinearLayout, f, tag).commit();
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.menufragment_layout, container, false);
+
+            aboutbutton = (Button) rootView.findViewById(R.id.AboutButton);
+            aboutbutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Perform action on click
+                    displayFragment(new AboutFragment(), AboutFragment.class.getSimpleName());
+                }
+            });
+
+            processbutton = (Button) rootView.findViewById(R.id.ProcessButton);
+            processbutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Perform action on click
+                    displayFragment(new ProcessFragment(), ProcessFragment.class.getSimpleName());
+                }
+            });
+
+            peoplebutton = (Button) rootView.findViewById(R.id.PeopleButton);
+            peoplebutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Perform action on click
+                    displayFragment(new PeopleFragment(), PeopleFragment.class.getSimpleName());
+                }
+            });
+
+            return rootView;
+        }
+    }
+
+    public static class AboutFragment extends Fragment {
+        private final String TAG = AboutFragment.class.getSimpleName();
+        public AboutFragment() {
+
+        }
+
+    }
+
+    public static class ProcessFragment extends Fragment {
+        private final String TAG = AboutFragment.class.getSimpleName();
+        public ProcessFragment() {
+
+        }
+
+    }
+
+    public static class PeopleFragment extends Fragment {
+        private final String TAG = PeopleFragment.class.getSimpleName();
+        public PeopleFragment() {
+
+        }
+
     }
 }
